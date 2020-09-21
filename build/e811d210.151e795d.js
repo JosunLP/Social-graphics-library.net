5fb1467aa4b48848bb8087dbfc7868844d025fc2
j.pfalzgraf <j.pfalzgraf@ccvossel.de>
2020-09-21 15:52:52 +0200

    build

---
+++ build/e811d210.151e795d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{71:function(a){a.exports=JSON.parse('{"allTagsPath":"/socialGraphicsLibrary/blog/tags","slug":"update","name":"update","count":1,"permalink":"/socialGraphicsLibrary/blog/tags/update"}')}}]);
