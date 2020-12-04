23108a675dd2bc2e9d26ef986c2a0d8d4c309111
JosunLP <jolun.swtor@gmail.com>
2020-12-04 20:04:24 +0100

    adding test area

--- static/example.js
+++ static/example.js
@@ -1,15 +1,17 @@
 import "./dist/sgl.bundle.js";
 
+var sgl = new SocialGraphicsLibrary();
+
 document.getElementById("generate").addEventListener("click", async function() {
-    new SocialGraphicsLibrary.multiGenerator(document.getElementById("tName").value, document.getElementById("uName").value, [{
+    sgl.multiGenerator(document.getElementById("tName").value, document.getElementById("uName").value, [{
         mode: "logo",
         containerId: "img-container-1",
         imgMode: "webp",
         generateLink: true
     }, {
-        mode: "twitter-title",
+        mode: "twitch-title",
         containerId: "img-container-2",
-        imgMode: "jpeg	",
+        imgMode: "jpeg",
         generateLink: true
     }])
-})
+});
